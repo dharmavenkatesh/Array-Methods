4e5646f307e9732706f993b64cc70b65448d2660
dharmavenkatesh <84831982+dharmavenkatesh@users.noreply.github.com>
2021-10-31 10:31:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var arr=[1,2,3,4,5,6,7,8]
+
+//var new_arr=arr.slice(2,5)
+
+//console.log(new_arr)
+
+//var new_arr = []
+//for(var i=0;i<arr.length;i++){
+ // if(arr[i]%2==0)new_arr.push(arr[i])
+//}
+//var new_arr=arr.filter((element,index)=>{
+  //if(element > 0)return true
+//})
+//console.log(new_arr)
+
+arr.splice(3,3)
+console.log(arr)
